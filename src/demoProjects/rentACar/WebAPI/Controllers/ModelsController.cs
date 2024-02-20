14575e57cfc905ca23f2a03fe2084c0278f245ea
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-02-20 23:21:32 +0300

    4. Gün - Dynamic Search/Query

--- src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Models.Models;
 using Application.Features.Models.Queries.GetListModel;
+using Application.Features.Models.Queries.GetListModelByDynamic;
 using Core.Application.Requests;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +17,13 @@ namespace WebAPI.Controllers
         {
             GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest };
             ModelListModel result = await Mediator.Send(getListModelQuery);
+            return Ok(result);
+        }
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListModelByDynamicQuery getListModelByDynamicQuery = new GetListModelByDynamicQuery { PageRequest = pageRequest, Dynamic=dynamic };
+            ModelListModel result = await Mediator.Send(getListModelByDynamicQuery);
             return Ok(result);
         }
     }
